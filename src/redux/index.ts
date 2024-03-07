7a95b4be73e6f0c7922a797774f5d7fe10abf12a
Alice R <alicepr@yandex.ru>
2024-03-07 23:00:32 +0300

    feat(modal): create Modal slice and selectors

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,11 +2,15 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { ingredientApi } from './services/ingredientApi';
 import { burgerSlice } from './ui/burger/index';
 import { ordersApi } from './services/feedApi';
+import { userSlice } from './ui/user/index';
+import { modalSlice } from './ui/modal/index';
 
 const rootReducer = combineReducers({
   [ingredientApi.reducerPath]: ingredientApi.reducer,
   [ordersApi.reducerPath]: ordersApi.reducer,
   burger: burgerSlice.reducer,
+  user: userSlice.reducer,
+  modal: modalSlice.reducer,
 });
 
 export const store = configureStore({
---
+++ src/redux/ui/modal/index.ts
@@ -0,0 +1,96 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { loginThunk } from '../user/thunks/login-thunk';
+
+type ModalContent =
+  | 'ingredient'
+  | 'constructor'
+  | 'order'
+  | 'mobile-menu'
+  | 'loader'
+  | 'success'
+  | 'error'
+  | 'order-received';
+
+interface ModalState {
+  isModalOpen: boolean;
+  modalContent: ModalContent | null;
+  message: string | null;
+}
+
+export const initialState: ModalState = {
+  isModalOpen: false,
+  modalContent: null,
+  message: null,
+};
+
+export const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    showIngredient(state) {
+      state.modalContent = 'ingredient';
+      state.isModalOpen = true;
+    },
+    showConstructor(state) {
+      state.modalContent = 'constructor';
+      state.isModalOpen = true;
+    },
+    showOrder(state) {
+      state.modalContent = 'order';
+      state.isModalOpen = true;
+    },
+    showReceivedOrder(state) {
+      state.modalContent = 'order-received';
+      state.isModalOpen = true;
+    },
+    showMobileMenu(state) {
+      state.modalContent = 'mobile-menu';
+      state.isModalOpen = true;
+    },
+    showLoader(state) {
+      state.modalContent = 'loader';
+      state.isModalOpen = true;
+    },
+    showMessage(state, action: PayloadAction<string>) {
+      state.modalContent = 'success';
+      state.isModalOpen = true;
+      state.message = action.payload;
+    },
+    showError(state, action: PayloadAction<string>) {
+      state.modalContent = 'error';
+      state.isModalOpen = true;
+      state.message = action.payload;
+    },
+    closeModal(state) {
+      state.isModalOpen = false;
+      state.modalContent = null;
+      state.message = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // .addCase(submitOrderThunk.fulfilled, (state) => {
+      //   state.modalContent = 'order';
+      //   state.isModalOpen = true;
+      // })
+      // .addCase(submitOrderThunk.pending, (state) => {
+      //   state.modalContent = 'loader';
+      //   state.isModalOpen = true;
+      // })
+      // .addCase(submitOrderThunk.rejected, (state) => {
+      //   state.message = 'Не удалось создать заказ. Попробуйте еще раз';
+      // })
+      // .addCase(fetchIngredientsThunk.rejected, (state) => {
+      //   state.message =
+      //     'Не удалось получить ингредиенты с сервера. Попробуйте перезагрузить страницу';
+      // })
+      .addCase(loginThunk.rejected, (state, action) => {
+        if (action.error.message === 'email or password are incorrect') {
+          state.message =
+            'Логин или пароль не найдены. Проверьте правильность введенных данных или воспользуйтесь ссылками ниже';
+        }
+      });
+  },
+});
+
+export const modalActions = modalSlice.actions;
